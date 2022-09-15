f959dc27c27ff58e1a5e757d12fff88e4f33b898
kheersagar <santparja@gmail.com>
2022-09-15 10:15:34 +0530

    changes app.js

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-// importing libraried
+// importing libraries
 const express = require("express");
 const app = express();
 require("dotenv").config();
-var session = require('express-session');
 const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-// sessions set-up
-var sessionVar;
-const oneDay = 1000 * 60 * 60 * 24;
-app.use(session({
-    secret:process.env.Secret,
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
-    resave: false
-}));
 
 const ejs = require("ejs");
 // const mongoose = require('mongoose');
