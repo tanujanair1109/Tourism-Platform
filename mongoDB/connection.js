b238ba4acaca9e57d9db585dcc9ab757f34d278a
tanujanair1109 <96572278+tanujanair1109@users.noreply.github.com>
2022-09-22 20:30:04 +0530

    Update connection.js
    Changed the codes to a more optimal form with proper import statements.

--- mongoDB/connection.js
+++ mongoDB/connection.js
@@ -1,20 +1,20 @@
-var express = require('express');
-const mongoose = require("mongoose");
-require("dotenv").config();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 
-function dbconnect(){
-  // let url = process.env.MONGO_URL||"mongodb+srv://admin-tushar:testpassword@firstcluster.vc5uokn.mongodb.net/cgtourism";
+const  dbconnect = () =>{
   let url = process.env.MONGO_URL;
   mongoose.connect(url, {
       useNewUrlParser: true,
   });
   const con= mongoose.connection;
-
   con.on('open', ()=> {
     console.log('<^^^Database Connected^^^>');
   });
 }
 
-module.exports = {dbconnect};
+export default dbconnect;
