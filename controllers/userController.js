db690896a7dd8dc02879eca6400238c92a09fbe7
Tushar Kashyap <84785149+DaNtE-tk@users.noreply.github.com>
2022-09-26 21:16:00 +0530

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ import generateToken from "../utilities/generateToken.js";
 export const registerUser = async (req,res)=>{
   try{
     const {email,password,mobile,role} = req.body;
-    if(!(email,password)){
+    if(!(email||password)){
       res.status(400).send("Insufficent input");
     }
     const old = await User.findOne({email});
@@ -25,14 +25,38 @@ export const registerUser = async (req,res)=>{
     }
   }catch(err){
     console.log(err);
-    res.status(500).send(err);
+  }
+  res.status(500).send("Internal server error");
+}
+
+export const createAdmin = async (req,res)=>{
+  try{
+    const {email,password,mobile,role} = req.body;
+    if(!(email||password||role)){
+      res.status(400).send("Insufficent input");
+    }
+    if(role=='superadmin'||'admin'||'blockadmin'){
+      let encryptedPassword = await bcrypt.hash(password, 10);
+      const data = await User.create({
+        email,
+        password:encryptedPassword,
+        mobile,
+        role
+      })
+      res.status(201).send(data);
+    }else{
+      res.status(400).send("Invalid input for role!");
+    }
+  }catch(err){
+    console.log(err);
+  res.status(500).send("Internal server error");
   }
 }
 
 export const loginUser = async (req,res)=>{
   try{
     const {email,password} = req.body;
-    if(!(email,password)){
+    if(!(email||password)){
       res.status(400).send("Invalid credentials");
     }
       const user = await User.findOne({email});
@@ -47,7 +71,7 @@ export const loginUser = async (req,res)=>{
   }
   }catch(err){
     console.log(err);
-    res.status(500).send(err);
+    res.status(500).send("Internal server error");
   }
 }
 
@@ -61,7 +85,42 @@ export const userProfile = async (req,res)=>{
       res.status(200).send({data:user});
     }
   }catch(err){
-    res.status(500).send("Some error occured!");
     console.log(err);
+    res.status(500).send("Internal server error");
   }
-};
+}
+
+export const userUpdate = async (req,res)=>{
+  try{
+    const {email} = req.body;
+    const {password,mobile,role} = req.body;
+    let user = await User.findOne({email});
+    if(!user){
+      res.status(400).send("Invalid email");
+    }else{
+      let encryptedPassword = await bcrypt.hash(password, 10);
+      const data = await User.updateOne({email},{$set:{password:encryptedPassword,mobile:mobile,role:role}});
+      user = await User.findOne({email});
+      res.status(201).send(user);
+    }
+  }catch(err){
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
+}
+
+export const userDelete = async (req,res)=>{
+  try{
+    const {email} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      res.status(400).send("Invalid email");
+    }else{
+      await User.deleteOne({email});
+      res.status(202).send("User deleted!");
+    }
+  }catch(err){
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
+}
